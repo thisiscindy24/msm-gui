aa0385ecd77ce1ed8aeca0164042fd14170d1978
thisiscindy24 <cindyannwu@gmail.com>
2025-02-24 22:15:19 +0000

    create movie record

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,28 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    # Fetch form parameters
+    movie_title = params.fetch("query_title")
+    movie_year = params.fetch("query_year")
+    movie_duration = params.fetch("query_duration")
+    movie_description = params.fetch("query_description")
+    movie_image = params.fetch("query_image")
+    movie_director_id = params.fetch("query_director_id")
+  
+    # Create a new movie record
+    new_movie = Movie.new
+    new_movie.title = movie_title
+    new_movie.year = movie_year
+    new_movie.duration = movie_duration
+    new_movie.description = movie_description
+    new_movie.image = movie_image
+    new_movie.director_id = movie_director_id
+
+    new_movie.save
+    @list_of_movies = Movie.order({ :created_at => :desc }) 
+    render({ :template => "movie_templates/index" })
+      
+  end
 end
